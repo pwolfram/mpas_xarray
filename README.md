84845bd87079a7b0ab0b329eeabe7a5684404fee
Milena Veneziani <milena@lanl.gov>
2016-04-19 01:20:29 -0600

    Added additional preprocess option to support times associated with MPAS timeSeriesStatsAM fields. Updated README and added test case.

--- README.md
+++ README.md
@@ -5,7 +5,10 @@ Wrapper to handle importing MPAS files into xarray (https://github.com/pydata/xa
  Module does
  1. Converts MPAS "xtime" to xarray time.  Time dimension is assigned via
     `preprocess_mpas`.
- 2. Provides capability to remove redundant time entries from
+ 2. Converts MPAS "timeSinceStartOfSim" to xarray time optionally for MPAS fields coming from the
+    timeSeriesStatsAM.  Time dimension is assigned via 
+    `preprocess_mpas_timeSeriesStats`.
+ 3. Provides capability to remove redundant time entries from
     reading of multiple netCDF datasets via
     `remove_repeated_time_index`.
 
@@ -26,4 +29,24 @@ python mpas_xarray.py -f "globalStats*nc"
 ```
 
 This outputs a simple time-series plot of Time vs. Time to test functionality.
+
+ Example Usage for timeSeriesStatsAM fields:
+
+```
+from mpas_xarray import preprocess_mpas_timeSeriesStats, remove_repeated_time_index
+
+ds = xarray.open_mfdataset('am.mpas-cice*nc', preprocess=preprocess_mpas_timeSeriesStats)
+ds = remove_repeated_time_index(ds)
+```
+
+To test:
+
+```
+tar xzvf am.mpas-ciceShort.tgz
+python mpas_xarray.py -f "am.mpas-cice*nc" --istimeavg "true"
+```
+
+This plots a short time series of global average ice concentration, showing the correctly centered curve
+(derived using preprocess_mpas_timeSeriesStats) and the curve incorrectly shifted toward the end of the 
+time averaging period (derived using preprocess_mpas).
 
--- mpas_xarray.py
+++ mpas_xarray.py
@@ -28,7 +28,7 @@ import datetime
 import pandas as pd
 import xarray as xr
 
-def preprocess_mpas(ds, yearoffset=1700): #{{{
+def preprocess_mpas(ds, yearoffset=1849): #{{{
     """
     Builds corret time specification for MPAS, allowing a year offset because the
     time must be betwee 1678 and 2262 based on the xarray library.
@@ -53,6 +53,34 @@ def preprocess_mpas(ds, yearoffset=1700): #{{{
     ds.attrs.__setitem__('time_yearoffset',str(yearoffset))
 
     return ds #}}}
+
+def preprocess_mpas_timeSeriesStats(ds, yearoffset=1849): #{{{
+    """
+    Builds corret time specification for MPAS time series stats am fields,
+    allowing a year offset because the time must be betwee 1678 and 2262 
+    based on the xarray library.
+
+    Milena Veneziani
+    04/18/2016
+    """
+
+    daysSinceStart = ds.timeSeriesStatsMonthly_avg_daysSinceStartOfSim_1
+    time = [datetime.datetime(yearoffset,12,31) + datetime.timedelta(x) for x in daysSinceStart.values]
+    datetimes = pd.to_datetime(time)
+    # make sure  date times are set up properly
+    assert datetimes[0].year > 1678, 'ERROR: yearoffset=%s'%(yearoffset) + \
+            ' must be large enough to ensure datetimes larger than year 1678'
+    assert datetimes[-1].year < 2262, 'ERROR: yearoffset=%s'%(yearoffset) + \
+            ' must be large enough to ensure datetimes larger than year 2262'
+
+    # append the corret time information
+    ds.coords['Time'] = datetimes
+
+    # record the yroffset
+    ds.attrs.__setitem__('time_yearoffset',str(yearoffset))
+
+    return ds #}}}
+
 
 def remove_repeated_time_index(ds): #{{{
     """
@@ -91,16 +119,40 @@ def test_load_mpas_xarray_datasets(path):
 
     return
 
+def test_load_mpas_xarray_timeSeriesStats_datasets(path):
+    ds = xr.open_mfdataset(path, preprocess=preprocess_mpas_timeSeriesStats)
+    ds = remove_repeated_time_index(ds)
+    ds2 = xr.open_mfdataset(path, preprocess=preprocess_mpas)
+    ds2 = remove_repeated_time_index(ds2)
+
+    # make a simple plot from the data
+    var = ds["timeSeriesStatsMonthly_avg_iceAreaCell_1"]
+    var2 = ds2["timeSeriesStatsMonthly_avg_iceAreaCell_1"]
+    ind_ice  = var > 0
+    ind_ice2 = var2 > 0
+    var_ice  = var.where(ind_ice)
+    var_ice =  var_ice.mean('nCells')
+    var2_ice = var2.where(ind_ice2)
+    var2_ice = var2_ice.mean('nCells')
+    var_ice.plot()
+    var2_ice.plot()
+    plt.title("Curve centered around right times (b) \n Curve shifted towards end of avg period (g)")
+    plt.show()
+
+    return
 
 if __name__=="__main__":
     from optparse import OptionParser
 
     parser = OptionParser()
     parser.add_option("-f", "--file", dest="inputfilename", help="files to be opened with xarray, could be of form 'output*.nc'", metavar="FILE")
+    parser.add_option("--istimeavg", dest="istimeavg", help="option to use the preprocess for timeSeriesStatsAM fields")
 
     options, args = parser.parse_args()
     if not options.inputfilename:
         parser.error("Input filename or expression ('-f') is a required input... e.g., -f 'output*.npz'")
 
-    test_load_mpas_xarray_datasets(options.inputfilename)
-
+    if not options.istimeavg:
+	test_load_mpas_xarray_datasets(options.inputfilename)
+    else:
+	test_load_mpas_xarray_timeSeriesStats_datasets(options.inputfilename)
