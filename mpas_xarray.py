020cf05b07bad25a612d7262eea3a14987280555
Milena Veneziani <milena@lanl.gov>
2016-04-22 16:53:59 -0600

    Added preprocess option to support timeSeriesStats times using daysSinceStartOfSim (#6)
    * Added additional preprocess option to support times associated with
    MPAS timeSeriesStatsAM fields. Updated README and added test case.
    
    * Adds modelines, fixes whitespace, general cleanup
    
    * Updated test case for use of the timeSeriesStatsAM preprocessing to make it simpler to interpret.
    Added monthoffset and dayoffset for the timeSeriesStatsAM preprocessing.
    Added comments on choice of yearoffset, monthoffset and dayoffset.
    Made simple formatting changes.
    
    * Made test code for timeSeriesStats AM preprocessing more compact.
    
    * Cleaned up test case again.
    
    * Updates module doc string
    
    * Adds function to simplify assertions for datetimes
    
    * Updates doc string for meaning of yearoffset
    
    * PEP8 and style changes for clarity

--- mpas_xarray.py
+++ mpas_xarray.py
@@ -5,11 +5,12 @@ mpas_xarray.py
 Wrapper to handle importing MPAS files into xarray.
 
  Module does
- 1. Converts MPAS "xtime" to xarray-understood time dimension via
-    'preprocess_mpas'.
- 2. Provides capability to remove redundant time entries from
-    reading of multiple netCDF datasets via
-    'remove_repeated_time_index'.
+ 1. Converts MPAS "xtime" to xarray time.  Time dimension is assigned via
+    `preprocess_mpas`.
+ 2. Converts MPAS "timeSinceStartOfSim" to xarray time for MPAS fields coming from the
+    timeSeriesStatsAM.  Time dimension is assigned via `preprocess_mpas_timeSeriesStats`.
+ 3. Provides capability to remove redundant time entries from reading of multiple netCDF
+    datasets via `remove_repeated_time_index`.
 
  Example Usage:
 
@@ -22,35 +23,85 @@ Phillip J. Wolfram
 12/01/2015
 """
 
+import datetime
 import matplotlib.pyplot as plt
 import numpy as np
-import datetime
 import pandas as pd
 import xarray as xr
 
-def preprocess_mpas(ds, yearoffset=1700): #{{{
+def assert_valid_datetimes(datetimes, yearoffset): #{{{
     """
-    Builds corret time specification for MPAS, allowing a year offset because the
-    time must be betwee 1678 and 2262 based on the xarray library.
+    Ensure that datatimes are compatable with xarray
 
     Phillip J. Wolfram
-    12/01/2015
+    04/20/2016
     """
-
-    time = np.array([''.join(atime).strip() for atime in ds.xtime.values])
-    datetimes = [datetime.datetime(yearoffset + int(x[:4]), int(x[5:7]), \
-            int(x[8:10]), int(x[11:13]), int(x[14:16]), int(x[17:19])) for x in time]
-    # make sure  date times are set up properly
     assert datetimes[0].year > 1678, 'ERROR: yearoffset=%s'%(yearoffset) + \
             ' must be large enough to ensure datetimes larger than year 1678'
     assert datetimes[-1].year < 2262, 'ERROR: yearoffset=%s'%(yearoffset) + \
             ' must be large enough to ensure datetimes larger than year 2262'
 
+    return #}}}
+
+def preprocess_mpas(ds, yearoffset=1850): #{{{
+    """
+    Builds corret time specification for MPAS, allowing a year offset because the
+    time must be between 1678 and 2262 based on the xarray library.
+
+    The time specification is relevant for so-called time-slice model
+    experiments, in which CO2 and greenhouse gas conditions are kept
+    constant over the entire model simulation. Typical time-slice experiments
+    are run with 1850 (pre-industrial) conditions and 2000 (present-day)
+    conditions. Hence, a default date offset is chosen to be yearoffset=1849,
+    monthoffset=12, dayoffset=31 (day 1 of an 1850 run will be seen as
+    Jan 1st, 1850).
+
+    Phillip J. Wolfram
+    12/01/2015
+    """
+
+    # compute shifted datetimes
+    time = np.array([''.join(atime).strip() for atime in ds.xtime.values])
+    datetimes = [datetime.datetime(yearoffset + int(x[:4]), int(x[5:7]), \
+            int(x[8:10]), int(x[11:13]), int(x[14:16]), int(x[17:19])) for x in time]
+    assert_valid_datetimes(datetimes, yearoffset)
+
     # append the corret time information
     ds.coords['Time'] = pd.to_datetime(datetimes)
-
     # record the yroffset
-    ds.attrs.__setitem__('time_yearoffset',str(yearoffset))
+    ds.attrs.__setitem__('time_yearoffset', str(yearoffset))
+
+    return ds #}}}
+
+def preprocess_mpas_timeSeriesStats(ds, yearoffset=1849, monthoffset=12, dayoffset=31): #{{{
+    """
+    Builds corret time specification for MPAS timeSeriesStats analysis member fields,
+    allowing a date offset because the time must be between 1678 and 2262
+    based on the xarray library.
+
+    This time specification is relevant for so-called time-slice model
+    experiments, in which CO2 and greenhouse gas conditions are kept
+    constant over the entire model simulation. Typical time-slice experiments
+    are run with 1850 (pre-industrial) conditions and 2000 (present-day)
+    conditions. Hence, a default date offset is chosen to be yearoffset=1849,
+    monthoffset=12, dayoffset=31 (day 1 of an 1850 run will be seen as
+    Jan 1st, 1850).
+
+    Milena Veneziani
+    04/18/2016
+    """
+
+    # compute shifted datetimes
+    daysSinceStart = ds.timeSeriesStatsMonthly_avg_daysSinceStartOfSim_1
+    time = [datetime.datetime(yearoffset, monthoffset, dayoffset) + datetime.timedelta(x)
+            for x in daysSinceStart.values]
+    datetimes = pd.to_datetime(time)
+    assert_valid_datetimes(datetimes, yearoffset)
+
+    # append the corret time information
+    ds.coords['Time'] = datetimes
+    # record the yroffset
+    ds.attrs.__setitem__('time_yearoffset', str(yearoffset))
 
     return ds #}}}
 
@@ -66,22 +117,22 @@ def remove_repeated_time_index(ds): #{{{
     index = range(len(time))
     uniquetime = set()
     remove = []
-    for id, atime in enumerate(time):
+    for tid, atime in enumerate(time):
         if atime not in uniquetime:
             uniquetime.add(atime)
         else:
-            remove.append(id)
+            remove.append(tid)
 
     remove.reverse()
-    for id in remove:
-        index.pop(id)
+    for tid in remove:
+        index.pop(tid)
 
     # remove repeated indices
     ds = ds.isel(Time=index)
 
     return ds #}}}
 
-def test_load_mpas_xarray_datasets(path):
+def test_load_mpas_xarray_datasets(path): #{{{
     ds = xr.open_mfdataset(path, preprocess=preprocess_mpas)
     ds = remove_repeated_time_index(ds)
 
@@ -89,18 +140,46 @@ def test_load_mpas_xarray_datasets(path):
     ds.Time.plot()
     plt.show()
 
-    return
+    return #}}}
+
+def test_load_mpas_xarray_timeSeriesStats_datasets(path): #{{{
+    ds = xr.open_mfdataset(path, preprocess=preprocess_mpas_timeSeriesStats)
+    ds = remove_repeated_time_index(ds)
+    ds2 = xr.open_mfdataset(path, preprocess=preprocess_mpas)
+    ds2 = remove_repeated_time_index(ds2)
+
+    # make a simple plot from the data
+    def plot_data(ds):
+        var = ds["timeSeriesStatsMonthly_avg_iceAreaCell_1"]
+        return var.where(var > 0).mean('nCells').plot()
+
+    plot_data(ds)
+    plot_data(ds2)
+    plt.title("Curve centered around right times (b) \n "+\
+              "Curve shifted towards end of avg period (g)")
+    plt.show()
+
+    return #}}}
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     from optparse import OptionParser
 
     parser = OptionParser()
-    parser.add_option("-f", "--file", dest="inputfilename", help="files to be opened with xarray, could be of form 'output*.nc'", metavar="FILE")
+    parser.add_option("-f", "--file", dest="inputfilename",
+                      help="files to be opened with xarray, could be of form 'output*.nc'", \
+                      metavar="FILE")
+    parser.add_option("--istimeavg", dest="istimeavg",
+                      help="option to use the preprocess for timeSeriesStatsAM fields")
 
     options, args = parser.parse_args()
     if not options.inputfilename:
-        parser.error("Input filename or expression ('-f') is a required input... e.g., -f 'output*.npz'")
+        parser.error("Input filename or expression ('-f') is a required input..."+\
+                " e.g., -f 'output*.npz'")
 
-    test_load_mpas_xarray_datasets(options.inputfilename)
+    if not options.istimeavg:
+        test_load_mpas_xarray_datasets(options.inputfilename)
+    else:
+        test_load_mpas_xarray_timeSeriesStats_datasets(options.inputfilename)
 
+# vim: ai ts=4 sts=4 et sw=4 ft=python
